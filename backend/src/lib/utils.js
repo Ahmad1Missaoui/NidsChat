877233ed4d963a74adf9696911e8e297a04b2e9f
Ahmed1Missaoui <missaouiahmed404@gmail.com>
2025-10-19 16:11:41 +0200

    signup controller added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+
+export const generateToken = (UserId,res) => {
+  const token = jwt.sign({UserId},process.env.JWT_SECRET,{
+     expiresIn:"7d",
+
+
+    });
+
+    res.cookie("token",token,{
+        maxAge:7*24*60*60*1000, // 7 days
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production" ? true : false,
+        sameSite:"strict",
+
+  });
+
+
+
+}
