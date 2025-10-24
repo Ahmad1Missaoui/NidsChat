d67dd944809d852c64db41ee92d0a179f1e62e92
Ahmed1Missaoui <missaouiahmed404@gmail.com>
2025-10-24 18:00:46 +0200

    login & logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,12 +30,9 @@ export const signup = async (req,res) => {
             password:hashedPassword,
         });
         if (newUser) {
-
             //before review CR
             // generateToken(newUser._id,res);
-            // await newUser.save();
-
-
+            // await newUser.save()
             //after review CR
             const savedUser = await newUser.save();
             generateToken(savedUser._id, res);
@@ -67,4 +64,29 @@ export const signup = async (req,res) => {
         console.log("Error in signup controller:",error); 
         res.status(500).json({message:"Server error"});
     }
+};
+export const login = async (req,res) => {
+const {email,password} = req.body; 
+try{
+    const user = await User.findOne({email});
+     if(!user){ return res.status(400).json({message:"Invalid email or password"});}
+     const isPasswordCorrect = await bcrypt.compare(password,user.password);
+     if(!isPasswordCorrect){ return res.status(400).json({message:"Invalid email or password"});}
+        generateToken(user._id,res);
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic,
+        });
+
+    }catch(error){
+        console.log("Error in login controller:",error);
+        res.status(500).json({message:" Internal server error"});
+    }
+};
+  
+export const logout = (_,res) => { 
+   res.cookie("jwt","",{maxAge:0})
+   res.status(200).json({message:"Logged out successfully"});
 };
