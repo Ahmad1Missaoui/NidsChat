88261feb8a92c5a89e9d6de1768c885130cde253
Ahmed1Missaoui <missaouiahmed404@gmail.com>
2026-01-22 18:30:49 +0100

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,9 +4,12 @@ import { login } from '../controllers/auth.controller.js';
 import { logout } from '../controllers/auth.controller.js';
 import { updateProfile } from '../controllers/auth.controller.js';
 import { ProtectRoute } from '../middleware/auth.middleware.js';
+import arcjet from '@arcjet/node';
+import { ArcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router =express.Router();
 
+router.use(ArcjetProtection);
 router.post("/signup",signup);
 router.post("/login",login)
 router.post("/logout",logout);
