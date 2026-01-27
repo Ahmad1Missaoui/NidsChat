b2e694ecb37883a5ad5bea5872286ece127a4cec
Ahmed1Missaoui <missaouiahmed404@gmail.com>
2026-01-27 21:50:55 +0100

    frontend codebase setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route,Routes } from 'react-router'
+import ChatsPage from './pages/ChatsPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+  <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+  {/* DECORATORS - GRID BG & GLOW SHAPES */}
+  {/* Dark Grid Background */}
+  <div className="absolute inset-0 bg-[linear-gradient(to_right,#1f1f1f_1px,transparent_1px),linear-gradient(to_bottom,#1f1f1f_1px,transparent_1px)] bg-[size:14px_24px]" />
+  {/* Yellow and Black Glow Shapes */}
+  <div className="absolute top-0 -left-4 size-96 bg-green-400 opacity-20 blur-[100px]" />
+  <div className="absolute bottom-0 -right-4 size-96  bg-yellow-400 opacity-20 blur-[100px]"/>
+
+    <Routes>
+        <Route path="/" element={<ChatsPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/> 
+    </Routes>
+
+    </div>
+  );
 }
 
 export default App
