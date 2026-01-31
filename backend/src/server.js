9c735e26205789238aec21e55e4baf762bc7b9a7
Ahmed1Missaoui <missaouiahmed404@gmail.com>
2026-01-31 16:07:10 +0100

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import Path from 'path';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 
 dotenv.config();
 
@@ -15,8 +15,13 @@ const __dirname = Path.resolve();
 
 const PORT =ENV.PORT  || 3000;
 
-app.use(cookieParser());
+
 app.use(express.json());
+app.use(cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+}));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", MessageRoutes);
